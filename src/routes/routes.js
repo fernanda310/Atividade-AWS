8f28efd7774975a7a84433fcdd87114c93ea23e5
fernanda <101983708+fernanda310@users.noreply.github.com>
2025-03-04 20:27:54 -0300

    modificando o routes
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,18 +9,23 @@ const UserControllerNova = require('../Estruturado/Controller/UsuarioController'
 const storage = multer.memoryStorage();  
 const upload = multer({ storage: storage });  
 
+//imagem 
 router.post('/novaImagem', ImageControllerNova.novaImagem);
 router.get('/imagens', ImageControllerNova.listarImagem);
 router.get('/imagem/:id', ImageControllerNova.buscarImagem);
 router.put('/editarImagem/:id', ImageControllerNova.atualizarImagem);
 router.delete('/apagarImagem/:id', ImageControllerNova.removerImagem);
 
+
+//usuario
 router.post('/novoUsuario', UserControllerNova.novaUsuario);
 router.get('/usuarios', UserControllerNova.listarUsuario);
 router.get('/usuario/:id', UserControllerNova.buscarUsuario);
 router.put('/editarUsuario/:id', UserControllerNova.atualizarUsuario);
 router.delete('/apagarUsuario/:id', UserControllerNova.removerUsuario);
 
+
+//aws
 router.get('/awscontroller/buscar', AWSController.buscarImagem);
 router.post('/awscontroller/enviar/:id', upload.single('imagem'), AWSController.enviarImagem);
 
